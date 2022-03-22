7426dcd4d45edd3fe5ed3d1388ccbd39e960faeb
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-22 17:30:24 +0000

    init

--- main.cs
+++ main.cs
@@ -1,7 +1,32 @@
+// Created by: Kenny Le
+// Created on: Mar 2022
+//
+// This program accepts user input
+
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+class Program 
+{
+    public static void Main (string[] args) 
+    {
+        // This function accepts user input
+        string streetName;
+        int streetNumber;
+        
+        Console.WriteLine ("This program gets a user's address");
+        Console.WriteLine("");
+
+        Console.Write("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine("Your info is: " + streetNumber + " " + streetName + ".");
+
+        Console.WriteLine("\nDone.");
+    }
+
   }
 }
